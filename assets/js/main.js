4a61061d2b6469b9a0e0f6748ea2a2b600de0cb8
TraAnhTho <thotra081105@gmail.com>
2024-05-21 22:17:10 +0700

    feat; add showdow box, responsive navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,8 @@
 alert("Chào mừng bạn đến Magic Code");
-{
-  /* 
-    <script type="text/javascript">
-      
-    </script> */
-}
+
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
+
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ["Language", "User"],
@@ -25,13 +21,16 @@ function drawChart() {
     legend: "none",
     pieSliceText: "label",
     slices: {
-      2: { offset: 0.2 },
-      4: { offset: 0.2 },
-      6: { offset: 0.2 },
-      8: { offset: 0.2 },
+      0: { color: "#F9E1E0", offset: 0.2 }, // Java
+      1: { color: "#FEADB9" }, // PHP
+      2: { color: "#C88BD9", offset: 0.2 }, // Python
+      3: { color: "#9799BA" }, // C++
+      4: { color: "#BC85A3", offset: 0.2 }, // C#
+      5: { color: "#F3DDB3" }, // HTML
+      6: { color: "#487ba6", offset: 0.2 }, // JS
+      7: { color: "#E17E76" }, // Other
     },
   };
-
   var chart = new google.visualization.PieChart(
     document.getElementById("piechart")
   );
