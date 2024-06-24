4493c6c0c23880f56ea600d45de1bf01b0205bf8
TraAnhTho <thotra081105@gmail.com>
2024-06-24 07:59:49 +0700

    abc

--- .vscode/SQL/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-mysqli_connect("localhost", "TraAnhTho", "081105", "magiccode");
-
-if (mysqli_connect_error()) {
-	echo "Connection Error.";
-} else {
-	echo "Database Connection Successfully.";
-}
-?>
